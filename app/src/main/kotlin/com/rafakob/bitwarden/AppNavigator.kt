6198f7c76f289e8f9455ff839f80f3b51982ec05
Rafal Kobylko <rafakob@gmail.com>
2020-10-01 20:39:59 +0200

    Update Deps

--- app/src/main/kotlin/com/rafakob/bitwarden/AppNavigator.kt
+++ app/src/main/kotlin/com/rafakob/bitwarden/AppNavigator.kt
@@ -5,20 +5,17 @@ import com.rafakob.bitwarden.base.Navigator
 import com.rafakob.bitwarden.main.MainActivity
 import com.rafakob.bitwarden.splash.SplashActivity
 import com.rafakob.bitwarden.startup.StartupActivity
-import org.jetbrains.anko.startActivity
+import splitties.activities.start
 import javax.inject.Inject
 
 class AppNavigator @Inject constructor() : Navigator {
 
-    override fun startSplash(context: Context) {
-        context.startActivity<SplashActivity>()
-    }
+    override fun startSplash(context: Context) =
+        context.start<SplashActivity>()
 
-    override fun startLogin(context: Context) {
-        context.startActivity<StartupActivity>()
-    }
+    override fun startLogin(context: Context) =
+        context.start<StartupActivity>()
 
-    override fun startMain(context: Context) {
-        context.startActivity<MainActivity>()
-    }
+    override fun startMain(context: Context) =
+        context.start<MainActivity>()
 }
